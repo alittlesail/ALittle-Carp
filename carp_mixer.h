5378fd93d0c46021f965b745cb1e2ccb15082977
xingfanou <757609414@qq.com>
2020-09-18 15:15:01 +0800

    添加mixer项目

---
+++ carp_mixer.h
@@ -0,0 +1,13 @@
+#ifndef CARP_MIXER_INCLUDED
+#define CARP_MIXER_INCLUDED (1)
+
+#define SOKOL_IMPL
+#include "sokol/sokol_audio.h"
+
+#define STB_DS_IMPLEMENTATION
+#define STBDS_NO_SHORT_NAMES
+#include "stb/stb_ds.h"
+
+#include "stb/stb_vorbis.c"
+
+#endif

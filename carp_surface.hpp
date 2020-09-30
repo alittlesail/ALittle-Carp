08e69d27cf682b5b9e0d5cdea2e405203cc2cba8
xingfanou <757609414@qq.com>
2020-09-30 17:43:45 +0800

    优化surface

---
+++ carp_surface.hpp
@@ -0,0 +1,225 @@
+#ifndef CARP_SURFACE_INCLUDED
+#define CARP_SURFACE_INCLUDED
+
+#include <memory>
+#include <string>
+#include <vector>
+
+struct CarpSurfaceRect
+{
+	int x = 0;
+	int y = 0;
+	int w = 0;
+	int h = 0;
+};
+
+// 图片像素大小固定为4个字节，格式为ARGB
+class CarpSurface
+{
+public:
+	CarpSurface(int width, int height)
+	{
+		if (width <= 0 || height <= 0) return;
+
+		m_width = width;
+		m_height = height;
+		m_pixels.resize(width * height, 0);
+	}
+
+	int GetWidth() const { return m_width; }
+	int GetHeight() const { return m_height; }
+
+	void CopyFrom(CarpSurface* src, const CarpSurfaceRect* src_rect, int dst_x, int dst_y)
+	{
+		if (src == nullptr) return;
+
+		int src_x = 0;
+		int src_y = 0;
+		int src_width = src->m_width;
+		int src_height = src->m_height;
+		if (src_rect)
+		{
+			src_x = src_rect->x;
+			src_y = src_rect->y;
+			src_width = src_rect->w;
+			src_height = src_rect->h;
+		}
+
+		for (int row = 0; row < src_height; ++row)
+		{
+			const int src_row = row + src_y;
+			if (src_row < 0) continue;
+			if (src_row >= src->m_height) break;
+
+			const int dst_row = row + dst_y;
+			if (dst_row < 0) continue;
+			if (dst_row >= m_height) break;
+
+			for (int col = 0; col < src_width; ++col)
+			{
+				const int src_col = col + src_x;
+				if (src_col < 0) continue;
+				if (src_col >= src->m_width) break;
+
+				const int dst_col = col + dst_x;
+				if (dst_col < 0) continue;
+				if (dst_col >= m_width) break;
+
+				m_pixels[dst_row * m_width + dst_col] = src->m_pixels[src_row * src->m_width + src_col];
+			}
+		}
+	}
+
+	unsigned int* GetPixels() { return m_pixels.data(); }
+	unsigned int GetPitch() const { return m_width * sizeof(unsigned int); }
+
+	void ScaleFrom(CarpSurface* src, const CarpSurfaceRect* src_rect, const CarpSurfaceRect* dst_rect)
+	{
+		if (src == 0) return;
+
+		int src_x = 0;
+		int src_y = 0;
+		int src_width = src->m_width;
+		int src_height = src->m_height;
+		if (src_rect)
+		{
+			src_x = src_rect->x;
+			src_y = src_rect->y;
+			src_width = src_rect->w;
+			src_height = src_rect->h;
+		}
+
+		int dst_x = 0;
+		int dst_y = 0;
+		int dst_width = src->m_width;
+		int dst_height = src->m_height;
+		if (dst_rect)
+		{
+			dst_x = dst_rect->x;
+			dst_y = dst_rect->y;
+			dst_width = dst_rect->w;
+			dst_height = dst_rect->h;
+		}
+
+		// 如果宽高一致那么说明不需要缩放，直接进行拷贝即可
+		if (src_width == dst_width && src_height == dst_height)
+		{
+			CopyFrom(src, src_rect, dst_x, dst_y);
+			return;
+		}
+
+		const float ratio_width = static_cast<float>(src_width) / static_cast<float>(dst_width);
+		const float ratio_height = static_cast<float>(src_height) / static_cast<float>(dst_height);
+		for (int row = 0; row < dst_height; ++row)
+		{
+			const int src_row = static_cast<int>(row * ratio_height) + src_y;
+			if (src_row < 0) continue;
+			if (src_row >= src->m_height) break;
+
+			const int dst_row = row + dst_y;
+			if (dst_row < 0) continue;
+			if (dst_row >= m_height) break;
+
+			for (int col = 0; col < dst_width; ++col)
+			{
+				const int src_col = static_cast<int>(col * ratio_width) + src_x;
+				if (src_col < 0) continue;
+				if (src_col >= src->m_width) break;
+
+				const int dst_col = col + dst_x;
+				if (dst_col < 0) continue;
+				if (dst_col >= m_width) break;
+
+				m_pixels[dst_row * m_width + dst_col] = src->m_pixels[src_row * src->m_width + src_col];
+			}
+		}
+	}
+
+	unsigned int GetPixel(int x, int y)
+	{
+		if (x < 0 || y < 0 || x >= m_width || y >= m_height) return 0;
+		return m_pixels[y * m_width + x];
+	}
+
+	void SetPixel(int x, int y, unsigned int pixel)
+	{
+		if (x < 0 || y < 0 || x >= m_width || y >= m_height) return;
+		m_pixels[y * m_width + x] = pixel;
+	}
+
+	unsigned int GetGrid9(const char* type)
+	{
+		if (type == 0 || m_width == 0 || m_height == 0) return 0;
+
+		std::string new_type = type;
+
+		if (new_type == "left")
+		{
+			int col = m_width / 2;
+			while (col)
+			{
+				for (int row = 0; row < m_height; ++row)
+				{
+					if (m_pixels[row * m_width + col] != m_pixels[row * m_width + col - 1])
+						return col;
+				}
+				--col;
+			}
+			return col;
+		}
+
+		if (new_type == "right")
+		{
+			int col = m_width / 2;
+			while (col + 1 < m_width)
+			{
+				for (int row = 0; row < m_height; ++row)
+				{
+					if (m_pixels[row * m_width + col] != m_pixels[row * m_width + col + 1])
+						return col;
+				}
+				++col;
+			}
+			return col;
+		}
+
+		if (new_type == "top")
+		{
+			int row = m_height / 2;
+			while (row)
+			{
+				for (int col = 0; col < m_width; ++col)
+				{
+					if (m_pixels[row * m_width + col] != m_pixels[(row - 1) * m_width + col])
+						return row;
+				}
+				--row;
+			}
+			return row;
+		}
+
+		if (new_type == "bottom")
+		{
+			int row = m_height / 2;
+			while (row + 1 < m_height)
+			{
+				for (int col = 0; col < m_width; ++col)
+				{
+					if (m_pixels[row * m_width + col] != m_pixels[(row + 1) * m_width + col])
+						return row;
+				}
+				++row;
+			}
+			return row;
+		}
+
+		return 0;
+	}
+	
+private:
+	int m_width = 0;					// 图片宽度
+	int m_height = 0;					// 图片高度
+	std::vector<unsigned int> m_pixels;	// 像素内容
+};
+
+#endif
--- carp_surface_bind.hpp
+++ carp_surface_bind.hpp
@@ -4,7 +4,7 @@
 #include <stb/stb_image.h>
 #include <stb/stb_image_write.h>
 
-#include "carp_surface.h"
+#include "carp_surface.hpp"
 #include "carp_lua.hpp"
 #include "carp_rwops.hpp"
 #include "carp_string.hpp"
@@ -16,40 +16,46 @@ public:
 	{
 		luabridge::getGlobalNamespace(l_state)
 			.beginNamespace("carp")
-			.beginClass<Carp_Surface>("CarpSurface")
+			.beginClass<CarpSurface>("CarpSurface")
 			.endClass()
 
-			.addFunction("__CPPAPI_CreateSurface", Carp_CreateSurface)
-			.addFunction("__CPPAPI_FreeSurface", Carp_FreeSurface)
-			.addFunction("__CPPAPI_BlitSurface", BlitSurface)
-			.addFunction("__CPPAPI_LoadSurface", LoadSurfaceForLua)
-			.addFunction("__CPPAPI_SaveSurface", SaveSurface)
-			.addFunction("__CPPAPI_GetSurfaceWidth", GetSurfaceWidth)
-			.addFunction("__CPPAPI_GetSurfaceHeight", GetSurfaceHeight)
-			.addFunction("__CPPAPI_CutBlitSurface", CutBlitSurface)
-			.addFunction("__CPPAPI_GetSurfaceGrid9", GetSurfaceGrid9)
-			.addFunction("__CPPAPI_GetSurfacePixel", Carp_GetSurfacePixel)
-			.addFunction("__CPPAPI_SetSurfacePixel", Carp_SetSurfacePixel)
-			.addFunction("__CPPAPI_GetPixelAlpha", GetPixelAlpha)
-			.addFunction("__CPPAPI_SetPixelAlpha", SetPixelAlpha)
-			.addFunction("__CPPAPI_GetPixelRed", GetPixelRed)
-			.addFunction("__CPPAPI_SetPixelRed", SetPixelRed)
-			.addFunction("__CPPAPI_GetPixelGreen", GetPixelGreen)
-			.addFunction("__CPPAPI_SetPixelGreen", SetPixelGreen)
-			.addFunction("__CPPAPI_GetPixelBlue", GetPixelBlue)
-			.addFunction("__CPPAPI_SetPixelBlue", SetPixelBlue)
+			.addFunction("CreateCarpSurface", CreateCarpSurface)
+			.addFunction("FreeCarpSurface", FreeCarpSurface)
+			.addFunction("BlitCarpSurface", BlitCarpSurface)
+			.addFunction("LoadCarpSurface", LoadCarpSurfaceForLua)
+			.addFunction("SaveCarpSurface", SaveCarpSurface)
+			.addFunction("GetCarpSurfaceWidth", GetCarpSurfaceWidth)
+			.addFunction("GetCarpSurfaceHeight", GetCarpSurfaceHeight)
+			.addFunction("CutBlitCarpSurface", CutBlitCarpSurface)
+			.addFunction("GetCarpSurfaceGrid9", GetCarpSurfaceGrid9)
+			.addFunction("GetCarpSurfacePixel", GetCarpSurfacePixel)
+			.addFunction("SetCarpSurfacePixel", SetCarpSurfacePixel)
+			.addFunction("GetPixelAlpha", GetPixelAlpha)
+			.addFunction("SetPixelAlpha", SetPixelAlpha)
+			.addFunction("GetPixelRed", GetPixelRed)
+			.addFunction("SetPixelRed", SetPixelRed)
+			.addFunction("GetPixelGreen", GetPixelGreen)
+			.addFunction("SetPixelGreen", SetPixelGreen)
+			.addFunction("GetPixelBlue", GetPixelBlue)
+			.addFunction("SetPixelBlue", SetPixelBlue)
 		
 			.endNamespace();
 	}
 
 public:
-	/**
-	 * load surface
-	 * @param file_path
-	 * @return surface
-	 */
-	static Carp_Surface* LoadSurfaceForLua(const char* file_path) { return LoadSurface(file_path); }
-	static Carp_Surface* LoadSurface(const char* file_path)
+	static CarpSurface* CreateCarpSurface(int width, int height)
+	{
+		if (width <= 0 || height <= 0) return nullptr;
+		return new CarpSurface(width, height);
+	}
+
+	static void FreeCarpSurface(CarpSurface* surface)
+	{
+		delete surface;
+	}
+	
+	static CarpSurface* LoadCarpSurfaceForLua(const char* file_path) { return LoadSurface(file_path); }
+	static CarpSurface* LoadSurface(const char* file_path)
 	{
 		CarpLocalFile file;
 		file.SetPath(file_path);
@@ -57,7 +63,7 @@ public:
 		return LoadSurface(file);
 	}
 
-	static Carp_Surface* LoadSurface(const CarpLocalFile& file)
+	static CarpSurface* LoadSurface(const CarpLocalFile& file)
 	{
 		int width = 0;
 		int height = 0;
@@ -69,42 +75,29 @@ public:
 			stbi_image_free(uc);
 			return nullptr;
 		}
-		Carp_Surface* surface = Carp_CreateSurface(width, height);
-		if (surface) memcpy(surface->pixels, uc, width * height * comp);
+		CarpSurface* surface = CreateCarpSurface(width, height);
+		if (surface) memcpy(surface->GetPixels(), uc, width * height * comp);
 		stbi_image_free(uc);
 		return surface;
 	}
 
-	static bool SaveSurface(Carp_Surface* surface, const char* file_path)
+	static bool SaveCarpSurface(CarpSurface* surface, const char* file_path)
 	{
-		return stbi_write_png(file_path, surface->w, surface->h, 4, surface->pixels, surface->w * 4) != 0;
+		return stbi_write_png(file_path, surface->GetWidth(), surface->GetHeight(), 4, surface->GetPixels(), surface->GetPitch()) != 0;
 	}
 
-	/**
-	 * get surface width
-	 * @return width
-	 */
-	static unsigned int GetSurfaceWidth(Carp_Surface* surface) { return surface->w; }
+	static unsigned int GetCarpSurfaceWidth(CarpSurface* surface) { return surface->GetWidth(); }
+	static unsigned int GetCarpSurfaceHeight(CarpSurface* surface) { return surface->GetHeight(); }
+	static unsigned int GetCarpSurfacePixel(CarpSurface* surface, int x, int y) { return surface->GetPixel(x, y); }
+	static void SetCarpSurfacePixel(CarpSurface* surface, int x, int y, unsigned int value) { surface->SetPixel(x, y, value); }
 
-	/**
-	 * get surface height
-	 * @return height
-	 */
-	static unsigned int GetSurfaceHeight(Carp_Surface* surface) { return surface->h; }
-
-	static void	BlitSurface(Carp_Surface* dest, Carp_Surface* src, unsigned int x, unsigned int y)
+	static void	BlitCarpSurface(CarpSurface* src, CarpSurface* dest, unsigned int x, unsigned int y)
 	{
-		Carp_CopySurface(src, nullptr, dest, x, y);
+		if (src == nullptr || dest == nullptr) return;
+		dest->CopyFrom(src, nullptr, x, y);
 	}
 
-	/**
-	 * copy surface
-	 * @param dest
-	 * @param src
-	 * @param from
-	 * @param to
-	 */
-	static bool	CutBlitSurface(Carp_Surface* dest, Carp_Surface* src, const char* to, const char* from)
+	static bool	CutBlitCarpSurface(CarpSurface* dest, CarpSurface* src, const char* to, const char* from)
 	{
 		std::vector<std::string> to_list;
 		CarpString::Split(to, ",", to_list);
@@ -114,95 +107,26 @@ public:
 		CarpString::Split(from, ",", from_list);
 		if (from_list.size() < 4) return false;
 
-		Carp_SurfaceRect dst_rect;
+		CarpSurfaceRect dst_rect;
 		dst_rect.x = atoi(to_list[0].c_str());
 		dst_rect.y = atoi(to_list[1].c_str());
 		dst_rect.w = atoi(to_list[2].c_str());
 		dst_rect.h = atoi(to_list[3].c_str());
 
-		Carp_SurfaceRect src_rect;
+		CarpSurfaceRect src_rect;
 		src_rect.x = atoi(from_list[0].c_str());
 		src_rect.y = atoi(from_list[1].c_str());
 		src_rect.w = atoi(from_list[2].c_str());
 		src_rect.h = atoi(from_list[3].c_str());
 
-		Carp_ScaleSurface(src, &src_rect, dest, &dst_rect);
+		dest->ScaleFrom(src, &src_rect, &dst_rect);
 		return true;
 	}
 
-	/**
-	 * Get scratchable latex four edges
-	 * @param surface
-	 * @param type left, right, top, bottom
-	 * @return edge size
-	 */
-	static unsigned int GetSurfaceGrid9(Carp_Surface* surface, const char* type)
+	static unsigned int GetCarpSurfaceGrid9(CarpSurface* surface, const char* type)
 	{
-		if (surface == 0 || type == 0 || surface->w == 0 || surface->h == 0) return 0;
-
-		std::string new_type = type;
-
-		if (new_type == "left")
-		{
-			int col = surface->w / 2;
-			while (col)
-			{
-				for (int row = 0; row < surface->h; ++row)
-				{
-					if (surface->pixels[row * surface->w + col] != surface->pixels[row * surface->w + col - 1])
-						return col;
-				}
-				--col;
-			}
-			return col;
-		}
-
-		if (new_type == "right")
-		{
-			int col = surface->w / 2;
-			while (col + 1 < surface->w)
-			{
-				for (int row = 0; row < surface->h; ++row)
-				{
-					if (surface->pixels[row * surface->w + col] != surface->pixels[row * surface->w + col + 1])
-						return col;
-				}
-				++col;
-			}
-			return col;
-		}
-
-		if (new_type == "top")
-		{
-			int row = surface->h / 2;
-			while (row)
-			{
-				for (int col = 0; col < surface->w; ++col)
-				{
-					if (surface->pixels[row * surface->w + col] != surface->pixels[(row - 1) * surface->w + col])
-						return row;
-				}
-				--row;
-			}
-			return row;
-		}
-
-		if (new_type == "bottom")
-		{
-			int row = surface->h / 2;
-			while (row + 1 < surface->h)
-			{
-				for (int col = 0; col < surface->w; ++col)
-				{
-					if (surface->pixels[row * surface->w + col] != surface->pixels[(row + 1) * surface->w + col])
-						return row;
-				}
-				++row;
-			}
-			return row;
-		}
-
-		return 0;
+		if (surface == nullptr) return 0;
+		return surface->GetGrid9(type);
 	}
 
 	static unsigned int GetPixelAlpha(unsigned int color) { return color >> 24; }

de6069d07220606ed14b4335718797e3e5b705a0
xingfanou <757609414@qq.com>
2020-11-13 10:39:15 +0800

    添加rudp客户端连接的实现

--- carp_rudp_client.hpp
+++ carp_rudp_client.hpp
@@ -30,11 +30,10 @@ public:
 	//连接部分/////////////////////////////////////////////////////////////////////////////////
 public:
 	/* 异步连接目标服务器
-	 * @param conv: 用于标识通讯双方，确保和服务器保持一致
 	 * @param ip: 目标服务器IP
 	 * @param port: 目标服务器端口
 	 */
-	void Connect(uint32_t conv, const std::string& ip, unsigned int port, asio::io_service* io_service
+	void Connect(const std::string& ip, unsigned int port, asio::io_service* io_service
 		, std::function<void()> failed_func, std::function<void()> succeed_func, std::function<void()> disconnected_func
 		, std::function<void(void*, int)> message_func)
 	{
@@ -52,20 +51,7 @@ public:
 		m_socket = std::make_shared<asio::ip::udp::socket>(*io_service);
 		// 创建一个目标服务器的连接点
 		m_endpoint = asio::ip::udp::endpoint(asio::ip::address_v4::from_string(ip), port);
-		// 创建kcp
-		m_kcp = ikcp_create(conv, this);
-		// 设置为流模式
-		m_kcp->stream = 1;
-		// 设置发送函数
-		m_kcp->output = UdpOutput;
-		// 设置为极速模式
-		ikcp_nodelay(m_kcp, 1, 10, 2, 1);
-
-		// 创建定时器
-		const int heartbeat = 20;
-		m_kcp_timer = std::make_shared<AsioTimer>(*io_service, std::chrono::milliseconds(heartbeat));
-		m_kcp_timer->async_wait(std::bind(&CarpRudpClient::UpdateKcp, this->shared_from_this(), std::placeholders::_1, heartbeat));
-
+		
 		// 保存并初始化
 		m_ip = ip;
 		m_port = port;
@@ -73,7 +59,9 @@ public:
 		// 打开socket
 		asio::error_code ec;
 		m_socket->open(asio::ip::udp::v4(), ec);
-		HandleAsyncConnect(ec);
+
+		// 发起连接
+		SendConnect(io_service);
 	}
 
 	// 判断是否已经连接
@@ -85,6 +73,17 @@ public:
 	// 关闭连接
 	void Close()
 	{
+		// 如果已连接成功，那么就发送关闭连接的协议
+		if (m_is_connected && m_socket && m_kcp)
+		{
+			const auto size = sizeof(int) + sizeof(uint32_t);
+			char buffer[size] = { 0 };
+			auto n_session = -m_session; // 这里使用负数的session来作为关闭操作
+			memcpy(buffer, &n_session, sizeof(int));
+			memcpy(buffer + sizeof(int), &m_kcp->conv, sizeof(uint32_t));
+			m_socket->send_to(asio::buffer(buffer, size), m_endpoint);
+		}
+		
 		// 释放带发送的消息包
 		for (auto& info : m_pocket_list) free(info.memory);
 		m_pocket_list.clear();
@@ -112,6 +111,11 @@ public:
 			m_kcp_timer->cancel();
 			m_kcp_timer = AsioTimerPtr();
 		}
+		if (m_connect_timer)
+		{
+			m_connect_timer->cancel();
+			m_connect_timer = AsioTimerPtr();
+		}
 
 		// 释放kcp
 		if (m_kcp)
@@ -119,15 +123,14 @@ public:
 			ikcp_release(m_kcp);
 			m_kcp = nullptr;
 		}
-
-		// 这里不要急着释放m_memory，可能asio正在用
-		// 放到析构函数里面释放
-		// if (m_memory) { free(m_memory); m_memory = 0; }
 	}
 
 private:
 	std::string m_ip;			// 目标服务器的IP
 	unsigned int m_port = 0;	// 目标服务器端口
+	int m_session = 0;
+	// 心跳包定时器
+	AsioTimerPtr m_connect_timer;	// 连接定时器
 
 public:
 	// 获取目标服务器IP和端口
@@ -135,9 +138,96 @@ public:
 	unsigned int GetPort() const { return m_port; }
 
 private:
-	// 异步连接
-	void HandleAsyncConnect(const asio::error_code& ec)
+	// 发起连接请求
+	void SendConnect(asio::io_service* io_service)
 	{
+		const auto size = sizeof(int) + sizeof(uint32_t);
+		void* memory = malloc(size);
+		// 这里讲session设置为0作为请求连接的操作
+		memset(memory, 0, size);
+		
+		m_socket->async_send_to(asio::buffer(memory, size), m_endpoint
+			, std::bind(&CarpRudpClient::HandleSendConnect, this->shared_from_this()
+				, std::placeholders::_1, std::placeholders::_2, memory));
+
+		// 等待接收连接应答包
+		m_socket->async_receive_from(asio::buffer(m_udp_buffer, sizeof(m_udp_buffer)), m_endpoint
+			, std::bind(&CarpRudpClient::HandleReceiveConnect, this->shared_from_this()
+				, std::placeholders::_1, std::placeholders::_2, io_service));
+
+		// 创建定时器
+		m_connect_timer = std::make_shared<AsioTimer>(*io_service, std::chrono::seconds(5));
+		m_connect_timer->async_wait(std::bind(&CarpRudpClient::HandleAsyncConnect, this->shared_from_this(), false));
+	}
+	void HandleSendConnect(const asio::error_code& ec, std::size_t actual_size, void* memory)
+	{
+		free(memory);
+	}
+	void HandleReceiveConnect(const asio::error_code& ec, std::size_t actual_size, asio::io_service* io_service)
+	{
+		if (ec)
+		{
+			HandleAsyncConnect(false);
+			return;
+		}
+
+		// 如果不是指定大小，那么就直接丢弃掉
+		const auto size = sizeof(int) + sizeof(uint32_t);
+		if (actual_size != size)
+		{
+			// 继续收包
+			m_socket->async_receive_from(asio::buffer(m_udp_buffer, sizeof(m_udp_buffer)), m_endpoint
+				, std::bind(&CarpRudpClient::HandleReceiveConnect, this->shared_from_this()
+					, std::placeholders::_1, std::placeholders::_2, io_service));
+			return;
+		}
+
+		// 取消定时器
+		if (m_connect_timer)
+		{
+			asio::error_code cancel_ec;
+			m_connect_timer->cancel(cancel_ec);
+			m_connect_timer = AsioTimerPtr();
+		}
+
+		// 取session
+		m_session = *reinterpret_cast<int*>(m_udp_buffer);
+		// 取conv
+		const auto conv = *reinterpret_cast<uint32_t*>(m_udp_buffer + sizeof(int));
+		// 检查是否连接成功
+		if (m_session <= 0 || conv == 0)
+		{
+			HandleAsyncConnect(false);
+			return;
+		}
+
+		// 创建kcp
+		m_kcp = ikcp_create(conv, this);
+		// 设置为流模式
+		m_kcp->stream = 1;
+		// 设置发送函数
+		m_kcp->output = UdpOutput;
+		// 设置为极速模式
+		ikcp_nodelay(m_kcp, 1, 10, 2, 1);
+		// 腾出四个字节用于保存session
+		ikcp_setmtu(m_kcp, static_cast<int>(m_kcp->mtu) - static_cast<int>(sizeof(int)));
+
+		// 创建定时器
+		const int heartbeat = 20;
+		m_kcp_timer = std::make_shared<AsioTimer>(*io_service, std::chrono::milliseconds(heartbeat));
+		m_kcp_timer->async_wait(std::bind(&CarpRudpClient::UpdateKcp, this->shared_from_this(), std::placeholders::_1, heartbeat));
+
+		HandleAsyncConnect(true);
+	}
+	// 处理连接超时
+	void HandleConnectTimeout(const asio::error_code& ec)
+	{
+		HandleAsyncConnect(false);
+	}
+	
+	// 异步连接
+	void HandleAsyncConnect(bool succeed)
+	{	
 		// 重置偏移
 		m_kcp_data_size = 0;
 		// 标记为不是正在连接
@@ -146,7 +236,7 @@ private:
 		m_executing = false;
 		// 标记为已连接
 		m_is_connected = false;
-		if (ec)
+		if (!succeed)
 		{
 			// 处理连接失败
 			HandleConnectFailed();
@@ -218,7 +308,34 @@ public:
 			return;
 		}
 
-		ikcp_input(m_kcp, m_udp_buffer, actual_size);
+		// 如果字节数不对，那么就直接丢弃
+		if (actual_size <= sizeof(int) + sizeof(uint32_t))
+		{
+			NextRead();
+			return;
+		}
+
+		// 取conv
+		const auto conv = ikcp_getconv(m_udp_buffer + sizeof(int));
+		// 如果conv和当前对不上，直接丢弃
+		if (conv != m_kcp->conv)
+		{
+			NextRead();
+			return;
+		}
+		
+		// 更新session
+		m_session = *reinterpret_cast<int*>(m_udp_buffer);
+		// 如果session小于0，那么说明是服务器主动断开连接
+		if (m_session <= 0)
+		{
+			ExecuteDisconnectCallback();
+			return;
+		}
+
+		// 把数据喂给kcp
+		ikcp_input(m_kcp, m_udp_buffer + sizeof(int), static_cast<int>(actual_size - sizeof(int)));
+		// 继续接收下一个数据包
 		NextRead();
 
 		// 接收所有可以接收的数据
@@ -278,7 +395,7 @@ private:
 	void UpdateKcp(const asio::error_code& ec, int interval)
 	{
 		const auto cur_time = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-		ikcp_update(m_kcp, cur_time);
+		ikcp_update(m_kcp, static_cast<uint32_t>(cur_time));
 		
 		if (!m_kcp_timer) return;
 		m_kcp_timer->expires_after(std::chrono::milliseconds(interval));
@@ -300,7 +417,7 @@ public:
 	void SendPocket(void* memory, const int memory_size)
 	{
 		// 计算每次发送的最大字节数
-		const int max_size = m_kcp->mss * m_kcp->rcv_wnd;
+		const int max_size = static_cast<int>(m_kcp->mss * m_kcp->rcv_wnd);
 
 		// 将一个包按最大字节数进行切割
 		const char* body = static_cast<const char*>(memory);
@@ -356,12 +473,15 @@ private:
 	{
 		auto* self = static_cast<CarpRudpClient*>(user);
 		// 发送
-		void* memory = malloc(len);
-		memcpy(memory, buf, len);
+		char* memory = static_cast<char*>(malloc(len + sizeof(int)));
+		// 填充当前session
+		memcpy(memory, &self->m_session, sizeof(int));
+		// 填充发送数据
+		memcpy(memory + sizeof(int), buf, len);
 
 		// 构建内存结构
 		PocketInfo info;
-		info.memory_size = len;
+		info.memory_size = static_cast<int>(len + sizeof(int));
 		info.memory = memory;
 
 		// 添加到待发送列表

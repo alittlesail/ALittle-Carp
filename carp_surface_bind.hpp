2d0ac7dca62ac6230f7f18e675145b6e58f29d05
ALittleSail <757609414@qq.com>
2021-09-03 22:50:52 +0800

    代码优化

--- carp_surface_bind.hpp
+++ carp_surface_bind.hpp
@@ -180,6 +180,9 @@ public:
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#ifdef _WIN32
+#define __STDC_LIB_EXT1__
+#endif
 #include "stb/stb_image_write.h"
 #endif
 #endif

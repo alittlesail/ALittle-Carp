ff7eb1f4e6860740668d8f3f14ce093fafa7ec68
ALittleSail <757609414@qq.com>
2021-09-26 22:47:33 +0800

    添加控制接口

--- carp_wiringpi_bind.hpp
+++ carp_wiringpi_bind.hpp
@@ -7,6 +7,7 @@
 #include <softTone.h>
 #include <pcf8591.h>
 #include <pcf8574.h>
+#include <pca9685/src/pca9685.h>
 #include "carp_lua.hpp"
 
 class CarpWiringPiBind
@@ -21,6 +22,7 @@ public:
 			.addFunction("pinMode", pinMode)
 			.addFunction("softPwmCreate", softPwmCreate)
 			.addFunction("softPwmWrite", softPwmWrite)
+			.addFunction("pwmWrite", pwmWrite)
 			.addFunction("softToneCreate", softToneCreate)
 			.addFunction("softToneWrite", softToneWrite)
 			.addFunction("digitalWrite", digitalWrite)
@@ -35,8 +37,29 @@ public:
 			.addFunction("wiringPiI2CSetup", wiringPiI2CSetup)
 			.addFunction("wiringPiI2CWrite", wiringPiI2CWrite)
 			.addFunction("wiringPiI2CRead", wiringPiI2CRead)
+
+			.addFunction("pca9685Setup", pca9685Setup)
+			.addFunction("pca9685PWMFreq", pca9685PWMFreq)
+			.addFunction("pca9685PWMReset", pca9685PWMReset)
+			.addFunction("pca9685PWMWrite", pca9685PWMWrite)
+			.addFunction("pca9685PWMRead", Pca9685PWMRead)
+			.addFunction("pca9685FullOn", pca9685FullOn)
+			.addFunction("pca9685FullOff", pca9685FullOff)
 			
 			.endNamespace();
+	}
+
+	static int Pca9685PWMRead(lua_State* l_state)
+	{
+		int fd = (int)luaL_checkinteger(l_state, 1);
+		int pin = (int)luaL_checkinteger(l_state, 2);
+		int on = 0;
+		int off = 0;
+		pca9685PWMRead(fd, pin, &on, &off);
+
+		lua_pushinteger(l_state, on);
+		lua_pushinteger(l_state, off);
+		return 2;
 	}
 };
 
